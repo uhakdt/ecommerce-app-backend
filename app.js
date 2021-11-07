4d9a9dfc278ef222ac512b2bb1984f301b54e706
uhakdt <uhakdt@gmail.com>
2021-11-07 22:59:12 +0000

    forgot to add category.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // Routes
+app.use(require('./routes/category.js'));
 app.use(require('./routes/order.js'));
 app.use(require('./routes/product.js'));
 app.use(require('./routes/user.js'));
