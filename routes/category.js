ee0e5a16164d59c6c465e8c0eecf0d7d1901fe17
uhakdt <uhakdt@gmail.com>
2021-11-07 23:49:13 +0000

    Added Get Categories + Get Category endpoints for Category route.

--- routes/category.js
+++ routes/category.js
@@ -4,6 +4,51 @@ const request = require('request');
 
 const router = express.Router();
 
-let currentURL = process.env.URL_DEV;
+// GET CATEGORIES
+router.get('/api/v1/categories', async (req, res) => {
+  try {
+    const results = await db.query(`SELECT * FROM public."Category";`)
+
+    if (results.rowCount > 0) {
+      res.status(200).json({
+        status: "OK",
+        data: {
+          addresses: results.rows
+        }
+      });
+    } else {
+      res.status(204).json({
+        status: "No Results."
+      });
+    }
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+// GET CATEGORY
+router.get('/api/v1/category/:id', async (req, res) => {
+  try {
+    const result = await db.query(
+      'SELECT * FROM public."Category" WHERE id = $1;', [
+      req.params.id
+    ])
+    
+    if (result.rowCount > 0) {
+      res.status(200).json({
+        status: "OK",
+        data: {
+          address: result.rows[0]
+        }
+      })
+    } else {
+      res.status(204).json({
+        status: "ID did not match."
+      });
+    }
+  } catch (error) {
+    console.log(error)
+  }
+});
 
 module.exports = router;
